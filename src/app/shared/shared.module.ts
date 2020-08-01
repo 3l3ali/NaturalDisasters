75765d353532d0ad0f5c0ab1222a61f44ad9c1f9
Moe Alali <moe3l3ali@gmail.com>
2020-08-02 01:50:48 +0300

    banner shared component done

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BannerComponent } from './components/banner/banner.component';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [BannerComponent],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    BannerComponent
+  ]
+})
+export class SharedModule { }
